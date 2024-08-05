9d232dc92558e73d6812734c0057f5f393b4f74a
Rodion-Gimranov <rodiongimranov6@gmail.com>
2024-08-06 00:10:28 +0300

    Import new component

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,19 +1,8 @@
-ы
 <template>
     <main class="about_page_container">
         <div class="blocks_row block1">
             <AboutGallery />
-            <div class="text_content_container">
-                <div class="message_block">
-                    <p class="message_title">New personal site 🎉</p>
-                    <p class="message_text">
-                        If you're reading this... thank you! Happy to have you here in my little
-                        corner of the internet and I hope you're enjoying yourself.<br />
-                        I have plenty more coming and hope you'll stick around ❤️
-                    </p>
-                </div>
-                <span class="future_content_block">Here will be content</span>
-            </div>
+            <AboutTextMessage />
             <AboutMusicPlayer />
             <AboutMetadata />
             <AboutLogo3D />
@@ -27,6 +16,7 @@
 
 <script setup>
 import AboutGallery from "../components/aboutPageComponents/AboutGallery.vue";
+import AboutTextMessage from "../components/aboutPageComponents/AboutTextMessage.vue";
 import AboutMusicPlayer from "../components/aboutPageComponents/AboutMusicPlayer.vue";
 import AboutMetadata from "../components/aboutPageComponents/AboutMetadata.vue";
 import AboutLogo3D from "../components/aboutPageComponents/AboutLogo3D.vue";
@@ -77,59 +67,5 @@ import AboutQuotes from "../components/aboutPageComponents/AboutQuotes.vue";
 
 .block1 > :nth-child(5) {
     order: 5;
-}
-
-.text_content_container {
-    width: 326px;
-    height: 326px;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    gap: 20px;
-}
-
-.message_block {
-    width: 100%;
-    height: 100%;
-
-    padding: 20px;
-    border-radius: 30px;
-    background: $block_bg_color;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    gap: 10px;
-}
-
-.message_title {
-    font-size: 20px;
-    font-weight: 500;
-    color: $primary_white_color;
-}
-
-.message_text {
-    font-size: 17px;
-    font-weight: 400;
-    color: $text_gray_color;
-}
-
-.future_content_block {
-    width: 100%;
-    height: 100%;
-
-    border-radius: 30px;
-    border: 2px #2f2f2f dashed;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    font-size: 18px;
-    font-weight: 400;
-    color: #949494;
 }
 </style>
