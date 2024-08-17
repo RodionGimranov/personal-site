61a38e26a49cfb1621c7f3920e88c757c54642a8
Rodion-Gimranov <rodiongimranov6@gmail.com>
2024-08-17 14:10:06 +0300

    Add the 'explicit_content' variable

--- src/data/playlistDataList.js
+++ src/data/playlistDataList.js
@@ -5,6 +5,7 @@ export const playlistDataList = [
             "../assets/images/webp/album-images/Tyler-the-Creator-Flower-Boy.webp",
             import.meta.url
         ).href,
+        explicit_content: true,
         song_name: "See You Again",
         artist_name: "Tyler, the Creator",
         song_url: new URL("../assets/songs/See-You-Again.mp3", import.meta.url).href,
@@ -15,6 +16,7 @@ export const playlistDataList = [
             "../assets/images/webp/album-images/Bo-Burnham-Inside.webp",
             import.meta.url
         ).href,
+        explicit_content: true,
         song_name: "All Eyes On Me",
         artist_name: "Bo Burnham",
         song_url: new URL("../assets/songs/All-Eyes-On-Me.mp3", import.meta.url).href,
@@ -25,6 +27,7 @@ export const playlistDataList = [
             "../assets/images/webp/album-images/Actors-Acts-of-Worship.webp",
             import.meta.url
         ).href,
+        explicit_content: false,
         song_name: "Like Suicide",
         artist_name: "Actors",
         song_url: new URL("../assets/songs/Like-Suicide.mp3", import.meta.url).href,
@@ -35,6 +38,7 @@ export const playlistDataList = [
             "../assets/images/webp/album-images/Paul-Jones-My-Way-(Rarities).webp",
             import.meta.url
         ).href,
+        explicit_content: false,
         song_name: "Free Me",
         artist_name: "Paul Jones",
         song_url: new URL("../assets/songs/Free-Me.mp3", import.meta.url).href,
@@ -45,6 +49,7 @@ export const playlistDataList = [
             "../assets/images/webp/album-images/Raleigh-Ritchie-Andy-(2020).webp",
             import.meta.url
         ).href,
+        explicit_content: true,
         song_name: "Big & Scared",
         artist_name: "Raleigh Ritchie",
         song_url: new URL("../assets/songs/Big&Scared.mp3", import.meta.url).href,
