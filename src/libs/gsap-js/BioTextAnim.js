53740161b294ccf3dbf243158f447d3febb9dfa0
Rodion-Gimranov <rodiongimranov6@gmail.com>
2024-09-11 14:24:03 +0300

    Disable text fill animation for screens under 440px

--- src/libs/gsap-js/BioTextAnim.js
+++ src/libs/gsap-js/BioTextAnim.js
@@ -4,17 +4,36 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 export function BioTextAnim() {
     gsap.registerPlugin(ScrollTrigger);
 
-    gsap.to(".aboutMe_text span", {
-        scrollTrigger: {
-            trigger: ".home_filling_text_section",
-            start: "top 35%",
-            end: "bottom top",
-            scrub: 1,
-            pin: true,
-            pinSpacing: true,
-        },
-        ease: "none",
-        immediateRender: false,
-        backgroundSize: "100% 100%",
+    let bioTextTrigger;
+    let screenWidth = window.innerWidth;
+
+    const initAnimation = () => {
+        if (window.innerWidth > 440 && !bioTextTrigger) {
+            bioTextTrigger = gsap.to(".aboutMe_text span", {
+                scrollTrigger: {
+                    trigger: ".home_filling_text_section",
+                    start: "top 35%",
+                    end: "bottom top",
+                    scrub: 1,
+                    pin: true,
+                    pinSpacing: true,
+                },
+                ease: "none",
+                immediateRender: false,
+                backgroundSize: "100% 100%",
+            });
+        } else if (window.innerWidth <= 440 && bioTextTrigger) {
+            bioTextTrigger.scrollTrigger.kill();
+            bioTextTrigger = null;
+        }
+    };
+
+    initAnimation();
+
+    window.addEventListener("resize", () => {
+        if (Math.abs(window.innerWidth - screenWidth) > 10) {
+            screenWidth = window.innerWidth;
+            initAnimation();
+        }
     });
 }
