fc718bd6f94a4b9b5255f6cb0bde07dd848622e5
Rodion-Gimranov <rodiongimranov6@gmail.com>
2024-09-18 20:30:09 +0300

    Implement modal window logic

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,69 @@
 <template>
-    <Header v-if="!$route.meta.hideHeader" />
+    <Header />
     <router-view />
-    <Footer v-if="!$route.meta.hideFooter" />
+    <ChangelogModal v-show="showChangelogModal" @closeChangelogModal="closeChangelogModal" />
+    <ProjectModal
+        v-show="showProjectModal"
+        :project="selectedProjectData"
+        @closeProjectModal="closeProjectModal"
+    />
+    <Footer />
 </template>
 
 <script setup>
+import { ref, onMounted, onUnmounted, watchEffect } from "vue";
 import { RouterView } from "vue-router";
+import emitter from "./eventBus.js";
 
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
+
+import ChangelogModal from "./components/ChangelogModal.vue";
+import ProjectModal from "./components/ProjectModal.vue";
+
+const showChangelogModal = ref(false);
+const showProjectModal = ref(false);
+const selectedProjectData = ref(null);;
+
+const closeChangelogModal = () => {
+    showChangelogModal.value = false;
+};
+
+const closeProjectModal = () => {
+    showProjectModal.value = false;
+};
+
+const handleKeydown = (event) => {
+    if (event.key === "Escape") {
+        closeChangelogModal();
+        closeProjectModal();
+    }
+};
+
+onMounted(() => {
+    emitter.on("show-changelog-modal", () => {
+        showChangelogModal.value = true;
+    });
+
+    emitter.on("show-project-modal", (projectData) => {
+        selectedProjectData.value = projectData;
+        showProjectModal.value = true;
+    });
+
+    window.addEventListener("keydown", handleKeydown);
+});
+
+onUnmounted(() => {
+    window.removeEventListener("keydown", handleKeydown);
+});
+
+watchEffect(() => {
+    if (showChangelogModal.value || showProjectModal.value) {
+        document.body.style.overflowY = "hidden";
+    } else {
+        document.body.style.overflowY = "scroll";
+    }
+});
 </script>
 
 <style lang="scss">
