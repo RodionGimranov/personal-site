a93117906e19d676fcad97ae5a286d669fd2f6b0
Rodion Gimranov <rodiongimranov6@gmail.com>
2024-10-31 21:07:03 +0300

    Update the array data

--- src/data/updateDataList.js
+++ src/data/updateDataList.js
@@ -1,124 +1,50 @@
 export const updateDataList = [
     {
-        id: 0,
-        version: "1.0.0",
-        date: "18.01.2024",
-        description: [{ text: "Official release of the website" }],
-    },
-    {
         id: 1,
-        version: "1.0.1",
-        date: "19.01.2024",
-        description: [{ text: "Updated QR code" }, { text: "Added link to resume" }],
-    },
-    {
-        id: 2,
-        version: "1.1.0",
-        date: "20.01.2024",
+        version: "#14",
+        date: "November 1, 2024",
         description: [
-            { text: "Updated text content" },
-            { text: "Updated intro design for the 'Projects' and 'About Me' pages" },
-        ],
-    },
-    {
-        id: 3,
-        version: "1.1.1",
-        date: "22.01.2024",
-        description: [
-            { text: "Updated site header design" },
-            { text: "Added sounds and a counter button" },
-        ],
-    },
-    {
-        id: 4,
-        version: "1.2.0",
-        date: "04.02.2024",
-        description: [
-            { text: "Completely redesigned the 'About Me' page" },
-            { text: "Added various new features like a music player and other blocks" },
-        ],
-    },
-    {
-        id: 5,
-        version: "1.2.1",
-        date: "20.02.2024",
-        description: [
-            { text: "Added video covers for project cards" },
-            { text: "Bug fixes and minor improvements" },
-        ],
-    },
-    {
-        id: 6,
-        version: "1.2.2",
-        date: "20.04.2024",
-        description: [
-            { text: "Corrected visual errors" },
-            { text: "Fixed music player bugs" },
-            { text: " Added arrow controls for switching quotes" },
-        ],
-    },
-    {
-        id: 7,
-        version: "1.2.3",
-        date: "04.05.2024",
-        description: [
-            { text: "Updated descriptions for project cards" },
-            { text: "Bug fixes and minor improvements" },
-        ],
-    },
-    {
-        id: 8,
-        version: "1.3.0",
-        date: "03.06.2024",
-        description: [
-            { text: "Updated the website logo" },
-            { text: "Project videos moved from a local folder to Firebase Storage" },
-            { text: "Improved performance of the 'Projects' page, eliminating freezing issues" },
-        ],
-    },
-    {
-        id: 9,
-        version: "1.3.1",
-        date: "10.06.2024",
-        description: [
-            { text: "Added tooltips for physical icons" },
-            { text: "Bug fixes and minor improvements" },
-        ],
-    },
-    {
-        id: 10,
-        version: "2.0.0 - Major Update",
-        date: "01.08.2024",
-        description: [
-            { text: "Significant improvements to UX/UI" },
-            { text: "New project indicator added to the site header" },
-            { text: "Implemented sorting functionality for projects and updates" },
             {
-                text: "Complete rebuild of the website using Vue.js for enhanced performance and scalability",
+                title: "new changelog design",
+                text: [
+                    "After some time, I decided to move away from using a popup for the changelog. Now, the changelog is back on a dedicated page with a refined design and more detailed update descriptions - sometimes even including photos and videos.",
+                ],
             },
             {
-                text: "Enhanced project cards; clicking on them now opens a window with detailed project information",
+                title: "bug fixes and improvements",
+                text: [
+                    "- fixed minor technical and visual bugs",
+                    "- fixed issues with the drop-shadow display when hovering over skill cards",
+                    "- the logic for displaying project cards has been consolidated into a single component",
+                ],
+            },
+            {
+                title: "other updates",
+                text: ["- updated the site’s favicon", "- updated videos for project cards"],
             },
         ],
     },
     {
-        id: 11,
-        version: "2.0.1",
-        date: "03.09.2024",
+        id: 0,
+        version: "#13",
+        date: "August 1, 2024",
         description: [
-            { text: "Visual bugs resolved" },
-            { text: "Site performance optimized" },
-            { text: "Added a sound wave visualizer to the music player" },
-        ],
-    },
-    {
-        id: 12,
-        version: "2.0.2",
-        date: "18.09.2024",
-        description: [
-            { text: "Introduced Action Menu to enhance site interaction" },
-            { text: "Moved Changelog to a modal window for streamlined access" },
-            { text: "Optimized the logic and functionality of modal window operations" },
+            {
+                title: "new features and components",
+                text: [
+                    "- added a 'New Project' indicator in the site header",
+                    "- introduced an Action Menu to improve site interactions",
+                    "- enhanced project cards: clicking now opens a detailed project info window",
+                ],
+            },
+            {
+                title: "performance and UX/UI improvements",
+                text: [
+                    "- complete rebuild of the website using Vue.js for better performance and scalability",
+                    "- significant improvements to UX/UI across the site",
+                    "- optimized modal window logic and functionality",
+                ],
+            },
         ],
     },
 ];
