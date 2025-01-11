28b87815faafe613fcc7e978b4ae5a2e2d753053
Rodion Gimranov <rodiongimranov6@gmail.com>
2025-01-11 10:59:28 +0300

    Replace the usage of createWebHistory with createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import HomePage from "../views/HomePage.vue";
 import AboutPage from "../views/AboutPage.vue";
@@ -27,7 +27,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHistory("/personal-site/"),
+    history: createWebHashHistory(),
     routes,
 });
 
