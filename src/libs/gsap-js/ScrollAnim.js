6ccee738618e5d4613096624c266ed82a9a981ff
Rodion Gimranov <rodiongimranov6@gmail.com>
2025-01-14 10:47:28 +0300

    Add logic to disable animation

--- src/libs/gsap-js/ScrollAnim.js
+++ src/libs/gsap-js/ScrollAnim.js
@@ -4,27 +4,36 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 export function ScrollAnim() {
     gsap.registerPlugin(ScrollTrigger);
 
-    gsap.utils.toArray(".anim_section").forEach((section) => {
-        gsap.fromTo(
-            section,
-            {
-                scale: 0.7,
-                opacity: 0.2,
-                duration: 0.3,
-                y: 35,
-            },
-            {
-                scale: 1,
-                opacity: 1,
-                y: 0,
-                ease: "power2.out",
-                scrollTrigger: {
-                    trigger: section,
-                    start: "top 75%",
-                    end: "bottom 75%",
-                    scrub: 0.5,
-                },
-            }
-        );
-    });
+    function initAnimations() {
+        ScrollTrigger.getAll().forEach((trigger) => trigger.kill()); // Удаляем предыдущие триггеры
+
+        if (window.innerWidth > 1040) {
+            gsap.utils.toArray(".anim_section").forEach((section) => {
+                gsap.fromTo(
+                    section,
+                    {
+                        scale: 0.7,
+                        opacity: 0.2,
+                        duration: 0.3,
+                        y: 35,
+                    },
+                    {
+                        scale: 1,
+                        opacity: 1,
+                        y: 0,
+                        ease: "power2.out",
+                        scrollTrigger: {
+                            trigger: section,
+                            start: "top 75%",
+                            end: "bottom 75%",
+                            scrub: 0.5,
+                        },
+                    }
+                );
+            });
+        }
+    }
+
+    window.addEventListener("resize", initAnimations);
+    initAnimations();
 }
