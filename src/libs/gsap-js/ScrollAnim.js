38a3d9845dd9b0291f062a83e367b3a5e6386055
Rodion Gimranov <rodiongimranov6@gmail.com>
2025-01-14 10:55:59 +0300

    Update logic to disable animation

--- src/libs/gsap-js/ScrollAnim.js
+++ src/libs/gsap-js/ScrollAnim.js
@@ -4,32 +4,35 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 export function ScrollAnim() {
     gsap.registerPlugin(ScrollTrigger);
 
+    let triggers = []; 
+
     function initAnimations() {
-        ScrollTrigger.getAll().forEach((trigger) => trigger.kill()); // Удаляем предыдущие триггеры
+        triggers.forEach((trigger) => trigger.kill());
+        triggers = [];
 
         if (window.innerWidth > 1040) {
             gsap.utils.toArray(".anim_section").forEach((section) => {
-                gsap.fromTo(
-                    section,
-                    {
-                        scale: 0.7,
-                        opacity: 0.2,
-                        duration: 0.3,
-                        y: 35,
-                    },
-                    {
-                        scale: 1,
-                        opacity: 1,
-                        y: 0,
-                        ease: "power2.out",
-                        scrollTrigger: {
-                            trigger: section,
-                            start: "top 75%",
-                            end: "bottom 75%",
-                            scrub: 0.5,
+                let trigger = ScrollTrigger.create({
+                    trigger: section,
+                    start: "top 75%",
+                    end: "bottom 75%",
+                    scrub: 0.5,
+                    animation: gsap.fromTo(
+                        section,
+                        {
+                            scale: 0.7,
+                            opacity: 0.2,
+                            y: 35,
                         },
-                    }
-                );
+                        {
+                            scale: 1,
+                            opacity: 1,
+                            y: 0,
+                            ease: "power2.out",
+                        }
+                    ),
+                });
+                triggers.push(trigger); 
             });
         }
     }
