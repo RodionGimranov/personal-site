fbf4d6736b28d6a4673dc504be34d8db48eeb669
Rodion Gimranov <rodiongimranov6@gmail.com>
2025-01-25 19:08:57 +0300

    Add hideFooter to Projects router

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,7 @@ const routes = [
         path: "/Projects",
         name: "projectsPage",
         component: ProjectsPage,
-        meta: { title: "Projects" },
+        meta: { title: "Projects", hideFooter: true },
     },
 ];
 
