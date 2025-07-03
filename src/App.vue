ffe65b15af981163c1903350fa84f4515fd42b1d
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-07-03 15:58:38 +0300

    feat: add Footer component import and body styles

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,36 @@
 <template>
     <Header />
     <router-view />
+    <Footer />
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
 
-import Header from "@/components/layout/Header/Header.vue";
+import Header from "@/components/layout/Header.vue";
+import Footer from "@/components/layout/Footer.vue";
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+body {
+    width: 100vw;
+    height: 100vh;
+    min-width: 320px;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    background: $primary_white;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+
+    font-size: 16px;
+    font-weight: 400;
+    color: $primary_dark;
+    font-family: "Inter", sans-serif;
+    font-optical-sizing: auto;
+    font-style: normal;
+    line-height: 20px;
+}
+</style>
