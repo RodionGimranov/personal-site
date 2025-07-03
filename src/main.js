0c172bc5169fb23e9054c64c5821dc9e21e60bbd
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-07-03 15:58:57 +0300

    feat: add i18n library import

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
 import store from "@/store/store.js";
+import i18n from "@/libs/vue-i18n/vue-i18n.js";
 
-createApp(App).use(router).use(store).mount("#app");
+createApp(App).use(router).use(store).use(i18n).mount("#app");
