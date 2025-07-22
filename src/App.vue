31113e1fc0e87497c7581cde0512bdeb5663e3b2
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-07-22 18:50:13 +0300

    feat: update styles and import BlurMask component

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
             <div class="scrolled_content">
                 <router-view />
                 <Footer v-if="!$route.meta.hideFooter" />
+                <BlurMask />
             </div>
         </section>
     </main>
@@ -15,6 +16,7 @@ import { RouterView } from "vue-router";
 
 import SideBar from "@/components/layout/SideBar.vue";
 import Footer from "@/components/layout/Footer.vue";
+import BlurMask from "@/components/ui/BlurMask.vue";
 </script>
 
 <style lang="scss">
@@ -29,9 +31,11 @@ main {
 .main_content {
     display: flex;
     flex: 1 0 0px;
+    overflow: hidden;
 }
 
 .scrolled_content {
+    position: relative;
     width: 100%;
 }
 </style>
