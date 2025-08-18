58ecff332fd7483cdbd1b7b191326c072355cf26
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-08-18 23:41:43 +0500

    chore: update project metadata

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,11 @@ import { createStore } from "vuex";
 import modals from "@/store/modules/modals.js";
 import theme from "@/store/modules/theme.js";
 
-const appVersion = "0.3.40";
-const linesOfCode = 2975;
-const fileCount = 50;
-const folderCount = 22;
-const projectSizeMB = "87.49 mb";
+const appVersion = "0.3.44";
+const linesOfCode = 2985;
+const fileCount = 53;
+const folderCount = 25;
+const projectSizeMB = "87.57 mb";
 
 export default createStore({
     modules: {
