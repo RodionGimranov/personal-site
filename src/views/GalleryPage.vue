78ad06c49182ac7bdb4160f1974db8088c812c7b
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-08-24 12:59:41 +0300

    feat: add photo links

--- src/views/GalleryPage.vue
+++ src/views/GalleryPage.vue
@@ -6,22 +6,31 @@
                 :key="index"
                 class="masonry_item commom_card_style"
             >
-                <img class="gallery_image_item" :src="image" alt="Gallery image" />
+                <img class="gallery_image_item" :src="image" alt="Gallery image" loading="lazy" />
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
-// const images = [
-//     Add links
-// ];
+const images = [
+    "https://res.cloudinary.com/dii7e7hu0/image/upload/v1755964009/IMG_9794_flqlfc.jpg",
+    "https://res.cloudinary.com/dii7e7hu0/image/upload/v1755964008/IMG_9948_yxa0xw.jpg",
+    "https://res.cloudinary.com/dii7e7hu0/image/upload/v1755964008/IMG_9041_dny535.jpg",
+    "https://res.cloudinary.com/dii7e7hu0/image/upload/v1755964007/IMG_2592_a4lc1v.jpg",
+    "https://res.cloudinary.com/dii7e7hu0/image/upload/v1755964008/IMG_3419_csokwn.jpg",
+    "https://res.cloudinary.com/dii7e7hu0/image/upload/v1755964007/IMG_0653_nkkwe1.jpg",
+    "https://res.cloudinary.com/dii7e7hu0/image/upload/v1755964007/IMG_9327_rln2nr.jpg",
+    "https://res.cloudinary.com/dii7e7hu0/image/upload/v1755964006/IMG_6454_qu7meu.jpg",
+    "https://res.cloudinary.com/dii7e7hu0/image/upload/v1755964007/IMG_7262_i3ggrw.jpg",
+];
 </script>
 
 <style lang="scss">
 .gallery_page_wrapper {
     width: 100%;
     padding: 16px;
+    margin-bottom: 140px;
 }
 
 .masonry_container {
@@ -30,7 +39,6 @@
 }
 
 .masonry_item {
-    // or 16px
     border-radius: 16px;
     margin-bottom: 16px;
     break-inside: avoid;
