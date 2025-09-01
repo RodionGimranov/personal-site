0096c6586e364b7ac40f2ed8f36c5b83d3529d65
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-09-01 20:18:23 +0300

    refactor: move components into folders

---
+++ src/components/homeComponents/BioText.vue
@@ -0,0 +1,16 @@
+<template>
+    <p class="bio_text" v-html="$filters.formatText($t('message.bio_text'))"></p>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+.bio_text {
+    width: 100%;
+    font-size: 36px;
+    font-weight: 600;
+    color: $primary_dark;
+    text-align: center;
+    line-height: 44px;
+}
+</style>
---
+++ src/components/ui/cards/ProjectCard.vue
@@ -0,0 +1,90 @@
+<template>
+    <RouterLink :to="`/About-Project/${project.id}`" class="project_card_container">
+        <video
+            class="video_cover"
+            preload="metadata"
+            autoplay
+            loop
+            muted
+            playsinline
+            loading="lazy"
+        >
+            <source :src="project.project_cover" type="video/mp4" />
+            {{ $t("message.project_video_text") }}
+        </video>
+        <div class="project_name_container">
+            <SvgIcon name="eye-icon" />
+            <p class="project_name_text">{{ project.name }}</p>
+        </div>
+    </RouterLink>
+</template>
+
+<script setup>
+import SvgIcon from "@/components/ui/SvgIcon/SvgIcon.vue";
+
+defineProps({
+    project: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
+
+<style lang="scss">
+.project_card_container {
+    position: relative;
+    width: 515px;
+    height: 325px;
+    cursor: pointer;
+    overflow: hidden;
+    border-radius: 30px;
+    background: $fourth_gray;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.video_cover {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+.project_name_container {
+    position: absolute;
+    bottom: 12px;
+    left: 12px;
+    opacity: 0;
+    transition: 0.2s;
+    border-radius: 100px;
+    padding: 4px 12px 4px 6px;
+    backdrop-filter: blur(44px);
+    background: $primary_translucent_dark;
+
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 4px;
+}
+
+.project_card_container:hover .project_name_container {
+    opacity: 1;
+}
+
+.project_name_text {
+    font-size: 18px;
+    font-weight: 400;
+    color: $primary_white;
+}
+
+.dark_background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transition: 0.2s;
+}
+</style>
