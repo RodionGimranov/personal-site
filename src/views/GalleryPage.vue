0e0a7c31442b7baf09cec6fad1debc7f2365673f
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-09-11 19:21:17 +0300

    perf: optimize GalleryPage.vue component

--- src/views/GalleryPage.vue
+++ src/views/GalleryPage.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="gallery_page_wrapper">
         <div class="masonry_container">
-            <ImageCard :images="photos.photos" />
+            <ImageCard :images="shuffledPhotos" />
+        </div>
+        <div class="gallery_page_message">
+            <p>{{ $t("message.gallery_page_message") }}</p>
         </div>
     </div>
 </template>
@@ -9,7 +12,11 @@
 <script setup>
 import ImageCard from "@/components/ui/cards/ImageCard.vue";
 
+import { useShuffledPhotos } from "@/composables/useShuffledPhotos";
+
 import photos from "@/data/photos.json";
+
+const { shuffledPhotos } = useShuffledPhotos(photos.photos);
 </script>
 
 <style lang="scss">
@@ -21,5 +28,20 @@ import photos from "@/data/photos.json";
 .masonry_container {
     column-count: 4;
     column-gap: 16px;
+}
+
+.gallery_page_message {
+    width: 100%;
+    padding: 32px 16px 8px 16px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    p {
+        font-size: 14px;
+        font-weight: 400;
+        color: $secondary_gray;
+    }
 }
 </style>
