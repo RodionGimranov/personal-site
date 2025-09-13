13e6b544bcdcf59b6e14e8a143336bbd15866169
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-09-13 20:13:30 +0300

    feat: create TechnologyTag.vue component

---
+++ src/components/ui/TechnologyTag/TechnologyTag.vue
@@ -0,0 +1,26 @@
+<template>
+    <span class="technology_item">{{ name }}</span>
+</template>
+
+<script setup>
+defineProps({
+    name: { type: String, required: true },
+});
+</script>
+
+<style lang="scss">
+.technology_item {
+    padding: 4px 8px;
+    border-radius: 100px;
+    background: $secondary_blue;
+
+    font-size: 14px;
+    font-weight: 500;
+    color: $primary_blue;
+    white-space: nowrap;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
