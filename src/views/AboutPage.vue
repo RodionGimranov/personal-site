2520fd0d07f84e62f1de6fc3c0eebb6ec9c9545b
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-09-14 13:49:53 +0300

    refactor: add imports for new components in AboutPage.vue

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,19 +1,23 @@
 <template>
     <div class="about_page_wrapper common_page_wrapper">
         <div class="row_cards">
+            <GreetingCard />
             <LocationCard />
             <MusicPlayerCard />
         </div>
         <div class="row_cards">
             <ProjectStatsCard />
+            <EducationCard />
         </div>
     </div>
 </template>
 
 <script setup>
-import LocationCard from "../components/aboutComponents/LocationCard.vue";
+import GreetingCard from "@/components/aboutComponents/GreetingCard.vue";
+import LocationCard from "@/components/aboutComponents/LocationCard.vue";
 import MusicPlayerCard from "@/components/aboutComponents/MusicPlayerCard.vue";
-import ProjectStatsCard from "../components/aboutComponents/ProjectStatsCard.vue";
+import ProjectStatsCard from "@/components/aboutComponents/ProjectStatsCard.vue";
+import EducationCard from "@/components/aboutComponents/EducationCard.vue";
 </script>
 
 <style lang="scss">
