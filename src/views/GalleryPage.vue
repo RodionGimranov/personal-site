e9a4da9eb74b9799ca9d1523246434e28992bf8d
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-09-19 13:30:11 +0300

    perf: optimize GalleryPage component

--- src/views/GalleryPage.vue
+++ src/views/GalleryPage.vue
@@ -1,7 +1,19 @@
 <template>
     <div class="gallery_page_wrapper">
         <div class="masonry_container">
-            <ImageCard :images="shuffledPhotos" />
+            <ImageCard v-if="shuffledPhotos.length" :images="shuffledPhotos" />
+            <template v-else>
+                <Skeleton
+                    v-for="n in 8"
+                    :key="n"
+                    :style="{
+                        height: `${randomHeights[n - 1]}px`,
+                        marginBottom: '16px',
+                    }"
+                    width="100%"
+                    radius="16px"
+                />
+            </template>
         </div>
         <div class="gallery_page_message">
             <p>{{ $t("message.gallery_page_message") }}</p>
@@ -11,18 +23,24 @@
 
 <script setup>
 import ImageCard from "@/components/ui/cards/ImageCard.vue";
+import Skeleton from "@/components/ui/Skeleton/Skeleton.vue";
 
-import { useShuffledPhotos } from "@/composables/useShuffledPhotos";
+import { useShuffledPhotos } from "@/composables/useShuffledPhotos.js";
 
 import photos from "@/data/photos.json";
 
 const { shuffledPhotos } = useShuffledPhotos(photos.photos);
+
+const randomHeights = Array.from(
+    { length: 8 },
+    () => Math.floor(Math.random() * (470 - 355 + 1)) + 355,
+);
 </script>
 
 <style lang="scss">
 .gallery_page_wrapper {
     width: 100%;
-    padding: 16px;
+    padding: 6px 16px;
 }
 
 .masonry_container {
