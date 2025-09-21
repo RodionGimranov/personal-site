a8ba43e370abb3ebc311a493856e6b55a9d7f761
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-09-21 22:30:54 +0300

    feat: create PhysicsCard.vue component

---
+++ src/components/aboutComponents/PhysicsCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="physics_card_container common_bento_card_style" id="phycalsIcons"></div>
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+
+import { phycalsIcons } from "@/libs/matter-js/phycalsIcons.js";
+
+const physicsInstance = ref(null);
+
+onMounted(() => {
+    setTimeout(() => {
+        physicsInstance.value = phycalsIcons();
+    }, 100);
+});
+
+onUnmounted(() => {
+    if (physicsInstance.value && physicsInstance.value.destroy) {
+        physicsInstance.value.destroy();
+    }
+});
+</script>
+
+<style lang="scss">
+.physics_card_container {
+    width: 250px;
+    height: 520px;
+    padding: 0px !important;
+
+    canvas {
+        border-radius: inherit;
+    }
+}
+</style>
