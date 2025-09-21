1382b5a1555f6ff310174b7490a91cdc543892e6
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-09-21 23:46:24 +0300

    docs: update metadata

--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,13 @@ import { createStore } from "vuex";
 import modals from "@/store/modules/modals.js";
 import theme from "@/store/modules/theme.js";
 
-const appVersion = "0.7.66";
-const linesOfCode = 4626;
-// +8
-const fileCount = 80;
+const appVersion = "0.7.92";
+const linesOfCode = 4968;
+// +9
+const fileCount = 83;
 // +5
 const folderCount = 35;
-const projectSizeMB = 160.2;
+const projectSizeMB = 170.14;
 
 const resumeLink = "https://drive.google.com/drive/folders/1xIWBnyshRjCosq57ADyIZSv7d8wflI48?";
 
