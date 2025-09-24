87aa0d6eb5ae37e85272cc2399e1e48999fb75ac
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-09-24 18:14:28 +0300

    feat: update metadata and create figmaDesignLink

--- src/store/store.js
+++ src/store/store.js
@@ -3,15 +3,17 @@ import { createStore } from "vuex";
 import modals from "@/store/modules/modals.js";
 import theme from "@/store/modules/theme.js";
 
-const appVersion = "0.7.92";
-const linesOfCode = 4968;
+const appVersion = "0.8.42";
+const linesOfCode = 5053;
 // +9
-const fileCount = 83;
+const fileCount = 84;
 // +5
 const folderCount = 35;
-const projectSizeMB = 170.14;
+const projectSizeMB = 169.91;
 
 const resumeLink = "https://drive.google.com/drive/folders/1xIWBnyshRjCosq57ADyIZSv7d8wflI48?";
+const figmaDesignLink =
+    "https://www.figma.com/design/XwsuylKvqDHavXARZ4gBpH/Rodion-Gimranov-%7C-UI-Kit-%E2%80%93-Public?node-id=0-1&t=DXsPhWGaJGQXnwf6-1";
 
 export default createStore({
     modules: {
@@ -26,6 +28,7 @@ export default createStore({
             folderCount,
             projectSizeMB,
             resumeLink,
+            figmaDesignLink,
         };
     },
     getters: {
@@ -35,5 +38,6 @@ export default createStore({
         getFolderCount: (state) => state.folderCount,
         getProjectSizeMB: (state) => state.projectSizeMB,
         getResumeLink: (state) => state.resumeLink,
+        getFigmaDesignLink: (state) => state.figmaDesignLink,
     },
 });
