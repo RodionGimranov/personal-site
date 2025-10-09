d101d2fb49c8c29a1413ec52f0a32f61de9d471c
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-10-09 23:12:18 +0300

    feat: implement project display logic via useProjects

--- src/components/homeComponents/SelectedProjects.vue
+++ src/components/homeComponents/SelectedProjects.vue
@@ -1,5 +1,40 @@
-<template></template>
+<template>
+    <div class="selected_projects_section common_home_section_style">
+        <p class="common_home_section_title">
+            {{ $t("message.projects_home_section_title") }}
+        </p>
+        <div class="selected_projects_container">
+            <ProjectCard
+                v-for="project in selectedProjects"
+                :key="project.id"
+                :project="project"
+                projectWidth="320px"
+                projectHeight="206px"
+                projectNameSize="16px"
+            />
+        </div>
+    </div>
+</template>
 
-<script setup></script>
+<script setup>
+import { useProjects } from "@/composables/useProjects.js";
 
-<style lang="scss"></style>
+import ProjectCard from "@/components/ui/cards/ProjectCard.vue";
+
+const { getProjectsByIds } = useProjects();
+
+const selectedProjects = getProjectsByIds([0, 1]);
+</script>
+
+<style lang="scss">
+.selected_projects_section {
+    align-items: flex-start !important;
+}
+
+.selected_projects_container {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    gap: 21px;
+}
+</style>
--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -1,22 +1,17 @@
 <template>
     <div class="projects_page_wrapper common_page_wrapper">
-        <ProjectCard v-for="project in project" :key="project.id" :project="project" />
+        <ProjectCard v-for="project in allProjects" :key="project.id" :project="project" />
     </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
-import { useI18n } from "vue-i18n";
+import { useProjects } from "@/composables/useProjects.js";
 
 import ProjectCard from "@/components/ui/cards/ProjectCard.vue";
 
-import projects from "@/data/projects.json";
+const { projectsList } = useProjects();
 
-const { locale } = useI18n();
-
-const project = computed(() => {
-    return [...(projects[locale.value] || [])].sort((a, b) => b.id - a.id);
-});
+const allProjects = projectsList;
 </script>
 
 <style lang="scss">
