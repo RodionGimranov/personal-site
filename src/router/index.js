295087bc3503a91fb27a37c08b92d34a06add523
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-10-13 23:27:30 +0300

    refactor: update path names

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,42 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
-    { path: "/", redirect: "/Home" },
+    { path: "/", redirect: "/home" },
     {
-        path: "/Home",
+        path: "/home",
         name: "homePage",
         component: () => import("@/views/HomePage.vue"),
         meta: { titleKey: "message.my_name_title" },
     },
     {
-        path: "/About",
+        path: "/about",
         name: "aboutPage",
         component: () => import("@/views/AboutPage.vue"),
         meta: { titleKey: "message.about_tab_title" },
     },
     {
-        path: "/Projects",
+        path: "/projects",
         name: "projectsPage",
         component: () => import("@/views/ProjectsPage.vue"),
         meta: { titleKey: "message.projects_tab_title" },
     },
     {
-        path: "/About-Project/:id",
+        path: "/about-project/:id",
         name: "aboutProjectPage",
         component: () => import("@/views/AboutProjectPage.vue"),
         meta: { titleKey: "message.about_project_tab_title" },
     },
     {
-        path: "/Gallery",
+        path: "/gallery",
         name: "galleryPage",
         component: () => import("@/views/GalleryPage.vue"),
         meta: { titleKey: "message.gallery_tab_title", hideFooter: true },
     },
     {
-        path: "/Error",
-        name: "errorPage",
+        path: "/:pathMatch(.*)*",
+        name: "notFound",
         component: () => import("@/views/ErrorPage.vue"),
         meta: { titleKey: "message.error_tab_title", hideFooter: true },
-    },
-    {
-        path: "/:pathMatch(.*)*",
-        redirect: "/Error",
     },
 ];
 
