c4df35c133226d2c8038a883337618e1fe3f87ba
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-12-14 18:44:48 +0300

    refactor: update project display logic

--- src/components/homeComponents/SelectedProjects.vue
+++ src/components/homeComponents/SelectedProjects.vue
@@ -4,13 +4,34 @@
             {{ $t("message.projects_home_section_title") }}
         </p>
         <div class="selected_projects_container">
-            <ProjectCard projectWidth="320px" projectHeight="206px" projectNameSize="16px" />
+            <ProjectCard
+                v-for="project in projects"
+                :key="project.id"
+                :project="project"
+                projectWidth="320px"
+                projectHeight="206px"
+                projectNameSize="16px"
+            />
         </div>
     </div>
 </template>
 
 <script setup>
+import { watchEffect, computed } from "vue";
+import { useI18n } from "vue-i18n";
+
+import { useProjectsStore } from "@/stores/useProjectsStore.js";
+
 import ProjectCard from "@/components/ui/cards/ProjectCard.vue";
+
+const projectsStore = useProjectsStore();
+const { locale } = useI18n();
+
+watchEffect(() => {
+    projectsStore.setLocale(locale.value);
+});
+
+const projects = computed(() => projectsStore.getProjectsByIds([2, 1, 0]));
 </script>
 
 <style lang="scss">
--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -1,16 +1,31 @@
 <template>
-    <div class="projects_page_wrapper common_page_wrapper">
-        <!-- <ProjectCard /> -->
+    <div class="projects_page_wrapper">
+        <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
     </div>
 </template>
 
 <script setup>
+import { computed, watchEffect } from "vue";
+import { useI18n } from "vue-i18n";
+
+import { useProjectsStore } from "@/stores/useProjectsStore.js";
+
 import ProjectCard from "@/components/ui/cards/ProjectCard.vue";
+
+const projectsStore = useProjectsStore();
+
+const { locale } = useI18n();
+
+watchEffect(() => {
+    projectsStore.setLocale(locale.value);
+});
+
+const projects = computed(() => projectsStore.projects);
 </script>
 
 <style lang="scss">
 .projects_page_wrapper {
-    padding: 26px 36px 0 36px;
+    padding: 36px 36px 0 36px;
     display: flex;
     justify-content: flex-start;
     align-items: flex-start;
