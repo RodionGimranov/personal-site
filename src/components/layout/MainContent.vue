4e4786e5bd491545c0d9b55a9eaadf06e5989d3c
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-12-17 03:13:04 +0300

    feat: create MainContent.vue

---
+++ src/components/layout/MainContent.vue
@@ -0,0 +1,35 @@
+<template>
+    <section class="main_content_container common_layout_style">
+        <div class="scrolled_content">
+            <router-view />
+            <Footer v-if="!$route.meta.hideFooter" />
+        </div>
+    </section>
+</template>
+
+<script setup>
+import { RouterView, useRouter } from "vue-router";
+
+import Footer from "@/components/layout/Footer.vue";
+
+const router = useRouter();
+</script>
+
+<style lang="scss">
+.main_content_container {
+    position: relative;
+    overflow: hidden;
+
+    display: flex;
+    flex-direction: column;
+    flex: 1 0 0%;
+}
+
+.scrolled_content {
+    position: relative;
+    width: 100%;
+    margin: 10px 0;
+    overflow-x: hidden;
+    overflow-y: scroll;
+}
+</style>
