711b855003255196ce1bfbbdfab3d394b0ed7e27
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2025-12-18 09:20:44 +0300

    refactor: update appConstants.js

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -1,9 +1,9 @@
-export const APP_VERSION = "1.1.36";
+export const APP_VERSION = "1.1.40";
 
-export const TOTAL_LINES_OF_CODE = "5482";
+export const TOTAL_LINES_OF_CODE = "5486";
 export const TOTAL_FILE_COUNT = "77";
 export const TOTAL_FOLDER_COUNT = "32";
-export const PROJECT_SIZE_MB = "319.45";
+export const PROJECT_SIZE_MB = "319.99";
 
 export const RESUME_URL =
     "https://drive.google.com/drive/folders/1xIWBnyshRjCosq57ADyIZSv7d8wflI48?";
