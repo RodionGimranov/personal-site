dd53c5249c5aaf78612df2b88319db1c2cd6b3a3
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2026-01-08 21:20:04 +0300

    config: update appConstants file

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -1,9 +1,9 @@
-export const APP_VERSION = "1.2.59";
+export const APP_VERSION = "1.2.61";
 
-export const TOTAL_LINES_OF_CODE = "5489";
+export const TOTAL_LINES_OF_CODE = "5510";
 export const TOTAL_FILE_COUNT = "78";
 export const TOTAL_FOLDER_COUNT = "27";
-export const PROJECT_SIZE_MB = "365.15";
+export const PROJECT_SIZE_MB = "366.62";
 
 export const RESUME_URL =
     "https://drive.google.com/drive/folders/1xIWBnyshRjCosq57ADyIZSv7d8wflI48?";
