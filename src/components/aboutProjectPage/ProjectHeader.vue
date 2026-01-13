e77cb2dee9b9a37451486d869ff3085169657b48
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2026-01-13 18:50:39 +0300

    refactor: update href names

--- src/components/aboutProjectPage/ProjectHeader.vue
+++ src/components/aboutProjectPage/ProjectHeader.vue
@@ -2,10 +2,10 @@
     <div class="flex justify-between items-center mb-8! common_width_size">
         <p class="project_large_name text-[28px] font-medium text-primary-dark">{{ name }}</p>
         <div class="flex justify-start items-end gap-2.5">
-            <a :href="link_to_code" target="_blank">
+            <a :href="codeUrl" target="_blank">
                 <Button as="span" :buttonText="$t('message.source_code_btn')" />
             </a>
-            <a :href="link_to_deploy" target="_blank">
+            <a :href="deployUrl" target="_blank">
                 <Button as="span" variant="_blue" :buttonText="$t('message.visit_btn')" />
             </a>
         </div>
