7b2eadd61051f6e4a811d52e037884b2c0fe8db5
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2026-01-15 16:03:47 +0300

    refactor: migrate file extension from js to ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
 
 import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
     base: "/personal-site/",
-    plugins: [vue()],
+    plugins: [vue(), tailwindcss()],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
