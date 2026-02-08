9904e88b7dfcfd4b0cb9068c3e826dc69899146e
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2026-02-08 14:34:23 +0300

    feat: update appConstants definitions

--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -1,9 +1,9 @@
-export const APP_VERSION: string = "1.3.115";
+export const APP_VERSION: string = "1.3.215";
 
-export const TOTAL_LINES_OF_CODE: number = 5555;
-export const TOTAL_FILE_COUNT: number = 93;
+export const TOTAL_LINES_OF_CODE: number = 5574;
+export const TOTAL_FILE_COUNT: number = 96;
 export const TOTAL_FOLDER_COUNT: number = 29;
-export const PROJECT_SIZE_MB: number = 376.46;
+export const PROJECT_SIZE_MB: number = 376.51;
 
 export const RESUME_URL: string =
     "https://drive.google.com/drive/folders/1xIWBnyshRjCosq57ADyIZSv7d8wflI48?";
