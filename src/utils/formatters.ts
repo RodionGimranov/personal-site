1071cf0607ef416988001df26176f23c36d9fece
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2026-03-28 23:36:18 +0300

    docs: add comments

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,9 +1,9 @@
-// Transfer
+// Transfer Text
 export function formatTransferText(value: string): string {
     return value.replace(/\n/g, "<br />");
 }
 
-// Bold
+// Bold Text
 export function formatBoldText(value: string): string {
     return value.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>");
 }
