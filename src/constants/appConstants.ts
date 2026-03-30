45c498a1bd2d6c352e8d8342ab53afbf71c379ff
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2026-03-31 00:00:44 +0300

    feat: update appConstants definitions

--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -1,12 +1,17 @@
-export const APP_VERSION: string = "1.4.10";
+export const APP_VERSION: string = "1.4.32";
 
-export const TOTAL_LINES_OF_CODE: number = 5955;
-export const TOTAL_FILE_COUNT: number = 102;
+export const TOTAL_LINES_OF_CODE: number = 6050;
+export const TOTAL_FILE_COUNT: number = 100;
 export const TOTAL_FOLDER_COUNT: number = 31;
-export const PROJECT_SIZE_MB: number = 26.6;
+export const PROJECT_SIZE_MB: number = 0.6;
 
 export const RESUME_URL: string =
     "https://drive.google.com/drive/folders/1xIWBnyshRjCosq57ADyIZSv7d8wflI48?";
+
+export const TELEGRAM_URL = "https://t.me/RodionGimranov";
+export const GITHUB_URL = "https://github.com/RodionGimranov";
+export const INSTAGRAM_URL = "https://www.instagram.com/esta_laowai/";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/rodiongimranov/";
 
 export const FIGMA_DESIGN_URL: string =
     "https://www.figma.com/design/XwsuylKvqDHavXARZ4gBpH/Rodion-Gimranov-%7C-UI-Kit-%E2%80%93-Public?node-id=0-1&t=DXsPhWGaJGQXnwf6-1";
