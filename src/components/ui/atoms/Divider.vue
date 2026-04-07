174322126a1c5c2ff3f3329222fec8a7dd16e2b4
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2026-04-07 11:47:52 +0300

    feat: add Divider component

---
+++ src/components/ui/atoms/Divider.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="divider bg-black-10" :class="dividerClasses"></div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+type DividerType = "horizontal" | "vertical";
+
+const props = withDefaults(
+    defineProps<{
+        type?: DividerType;
+    }>(),
+    {
+        type: "horizontal",
+    },
+);
+
+const dividerClasses = computed(() => [`divider-${props.type}`]);
+</script>
+
+<style lang="scss">
+.divider-horizontal {
+    width: 100%;
+    min-height: 1px;
+    max-height: 1px;
+}
+.divider-vertical {
+    height: 100%;
+    min-width: 1px;
+    max-width: 1px;
+}
+</style>
