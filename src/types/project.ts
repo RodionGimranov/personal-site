963e180428fd916b71fe2425e8990e32c1b75b2d
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2026-04-18 17:58:38 +0300

    types: update type definitions

--- src/types/project.ts
+++ src/types/project.ts
@@ -2,19 +2,29 @@ export type ProjectAreaOfResponsibility = {
     area: string;
 };
 
-export type ProjectTechnology = {
-    name: string;
+export type ProjectMeta = {
+    project_video_cover: string;
+    link_to_deploy: string;
+    link_to_code: string;
 };
 
-export type Project = {
-    id: number;
+export type ProjectLocale = {
     name: string;
     description: string;
     role: string;
     areas_of_responsibility: ProjectAreaOfResponsibility[];
     context_purpose: string;
-    technologies: ProjectTechnology[];
-    project_video_cover: string;
-    link_to_deploy: string;
-    link_to_code: string;
+};
+
+export type ProjectI18n = {
+    ru: ProjectLocale;
+    en: ProjectLocale;
+};
+
+export type Project = {
+    id: number;
+    slug: string;
+    meta: ProjectMeta;
+    technologies: string[];
+    i18n: ProjectI18n;
 };
