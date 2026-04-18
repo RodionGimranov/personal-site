13117f3dc9a7aea69b57b83a172537f967037ed4
Rodion Gimranov <80162183+RodionGimranov@users.noreply.github.com>
2026-04-18 18:02:48 +0300

    i18n: update locales

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,25 +1,25 @@
 import ruGlobal from "./ru/global.json";
 import ruHome from "./ru/home.json";
 import ruAbout from "./ru/about.json";
-import ruProjectsLocale from "./ru/projects.locale.json";
+import ruProjects from "./ru/projects.json";
 
 import enGlobal from "./en/global.json";
 import enHome from "./en/home.json";
 import enAbout from "./en/about.json";
-import enProjectsLocale from "./en/projects.locale.json";
+import enProjects from "./en/projects.json";
 
 export const messages = {
     ru: {
         global: ruGlobal,
         home: ruHome,
         about: ruAbout,
-        projectsLocale: ruProjectsLocale,
+        projectsLocale: ruProjects,
     },
     en: {
         global: enGlobal,
         home: enHome,
         about: enAbout,
-        projectsLocale: enProjectsLocale,
+        projectsLocale: enProjects,
     },
 };
 
